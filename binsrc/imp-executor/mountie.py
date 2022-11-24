858f0aabefbfb495e8b289e2e57380de3b32a056
Alistair Young <avatar@arkane-systems.net>
2022-11-24 10:06:19 -0600

    Finish adding mount constants.

--- binsrc/imp-executor/mountie.py
+++ binsrc/imp-executor/mountie.py
@@ -15,16 +15,30 @@ MS_SYNCHRONOUS = 16         # Writes are synced at once.
 MS_REMOUNT = 32             # Alter flags of a mounted filesystem.
 MS_MANDLOCK = 64            # Allow mandatory locks on an FS.
 MS_DIRSYNC = 128            # Directory modifications are synchronous.
+MS_NOSYMFOLLOW = 256        # Do not follow symlinks.
+MS_NOATIME = 1024           # Do not update access times.
+MS_NODIRATIME = 2048        # Do not update directory access times.
 MS_BIND = 4096              # Bind directory at different place.
 MS_MOVE = 8192
 MS_REC = 16384
+MS_SILENT = 32768
+MS_POSIXACL = 1 << 16       # VFS does not apply the umask.
+MS_UNBINDABLE = 1 << 17     # Change to unbindable.
+MS_PRIVATE = 1 << 18        # Change to private.
+MS_SLAVE = 1 << 19          # Change to slave.
 MS_SHARED = 1 << 20         # Set propagation type to shared.
 MS_RELATIME = 1 << 21       # Update atime relative to mtime/ctime.
-MS_STRICTATIME = 1 << 24    # Always perform atime updates.  
+MS_KERNMOUNT = 1 << 22      # This is a kern_mount call.
+MS_I_VERSION =  1 << 23     # Update inode I_version field.
+MS_STRICTATIME = 1 << 24    # Always perform atime updates.
+MS_LAZYTIME = 1 << 25       # Update the on-disk [acm]times lazily.
+MS_ACTIVE = 1 << 30
+MS_NOUSER = 1 << 31
 
 
 # Unmount flags
 MU_NONE = 0                 # No flags.
+# ... TODO: add these
 
 
 libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
@@ -46,18 +60,3 @@ def unmount (target: str, flags: int = MU_NONE):
     if ret < 0:
         errno = ctypes.get_errno()
         raise OSError(errno, f"Error unmounting {target}: {os.strerror(errno)}")
-
-
-#     MS_NOSYMFOLLOW = 256,         /* Do not follow symlinks.  */
-#     MS_NOATIME = 1024,            /* Do not update access times.  */
-#     MS_NODIRATIME = 2048,         /* Do not update directory access times.  */
-#     MS_SILENT = 32768,
-#     MS_POSIXACL = 1 << 16,        /* VFS does not apply the umask.  */
-#     MS_UNBINDABLE = 1 << 17,      /* Change to unbindable.  */
-#     MS_PRIVATE = 1 << 18,         /* Change to private.  */
-#     MS_SLAVE = 1 << 19,           /* Change to slave.  */
-#     MS_KERNMOUNT = 1 << 22,       /* This is a kern_mount call.  */
-#     MS_I_VERSION =  1 << 23,      /* Update inode I_version field.  */
-#     MS_LAZYTIME = 1 << 25,        /* Update the on-disk [acm]times lazily.  */
-#     MS_ACTIVE = 1 << 30,
-#     MS_NOUSER = 1 << 31
